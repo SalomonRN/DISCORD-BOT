6acc7dd19e7eaf9363bee12a64f976152198d48e
SalomonRN <salomonrn20@gmail.com>
2025-05-19 22:20:56 -0500

    feat: enhance command handling for bot presence and reorganize event cogs

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import asyncio
 import discord
+from cogs.commands.logs_commands import LogCommands
+from cogs.commands.notify_commands import NotifyCommands
 from tasks.event_task import EventTask
 from discord.ext import commands
 import discord.ext
 import discord.ext.commands
 from os import getenv
 import utils.mongo as mongo
-from cogs.user_events import UserEvents
-from cogs.command_bot import CommandsBot
+from cogs.events.user_events import UserEvents
+from cogs.commands.command_bot import CommandsBot
+from cogs.events.server_events import ServerEvents
 from utils.mongo_utils import server_exist, create_server_in_db, change_active_status, update_server_info, delete_server_data
 from discord import app_commands
 
@@ -15,7 +18,6 @@ TOKEN = getenv("DISCORD_TOKEN")
 APP_ID = getenv("APP_ID")
 PUBLIC_KEY = getenv("PUBLIC_KEY")
 MY_SERVER_ID = getenv("MY_SERVER_ID")
-DEBUG = bool(getenv("DEBUG", False))
 MAIN_GUILD = None
 
 intents = discord.Intents.all()
@@ -40,13 +42,27 @@ async def on_ready():
 
 @bot.command()
 async def change(ctx: discord.ext.commands.Context):
-    if not ctx.author.resolved_permissions.administrator:
+    if not await bot.is_owner(ctx.author):
         return
-    
-    bot.command_prefix = ctx.message.content.split(" ")[1]
-    await ctx.send("Prefijo cambiado a: " + bot.command_prefix)
-
-
+    else:
+        activity = " ".join(ctx.message.content.split(" ")[2:]).capitalize()
+        match(ctx.message.content.split(" ")[1]):
+            case "play":       
+                game = discord.Game(name=activity)
+                await bot.change_presence(activity=game)
+            case "stream":
+                activity = " ".join(ctx.message.content.split(" ")[2:-1])
+                url = ctx.message.content.split(" ")[-1]
+                if url.islower():
+                    return await ctx.send("Vuelva a mandar el comando :), pero si ya cambió la actividad del bot ignora esto.", ephemeral=True)
+                await bot.change_presence(activity=discord.Streaming(name=activity, url=url))
+            case "listen":
+                await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity))
+            case "watch":
+                await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity))
+            case _:
+                print("ALGO MALIO SAL")  
+        
 @bot.event
 async def on_guild_join(guild: discord.Guild):
     """ Cuando el bot entra a un servidor """
@@ -73,7 +89,10 @@ async def on_guild_update(before: discord.Guild, after: discord.Guild):
         
 async def main():
     await bot.add_cog(UserEvents(bot))
-    await bot.add_cog(CommandsBot(bot, DEBUG))
+    await bot.add_cog(ServerEvents(bot))
+    await bot.add_cog(CommandsBot(bot))
+    await bot.add_cog(NotifyCommands(bot))
+    await bot.add_cog(LogCommands(bot))
     await bot.start(TOKEN)
 
 # await bot.run(TOKEN)
